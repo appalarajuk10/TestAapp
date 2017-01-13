eb63adc56f561a1dc94426abe3a2f5431a12af59
appalarajuk10 <appalarajuk10@gmail.com>
2017-01-13 23:24:31 +0530

    Created new project

---
+++ FirstAppVS2015/FirstAppVS2015Tests/ProgramTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstAppVS2015;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAppVS2015.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void WriteNameTest()
+        {
+            string name = "Raju";
+            Assert.AreEqual("Raju", name);
+        }
+
+        [TestMethod()]
+        public void combineArrayStringWithSpaceTest()
+        {
+
+            Program target = new Program(); // TODO: Initialize to an appropriate value
+            string[] stringArray = new string[] { "Today", "is", "the", "wonderful", "day", "of", "my", "life" }; // TODO: Initialize to an appropriate value
+            string expectedResult = "Today is the wonderful day of my life"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.combineArrayStringWithSpace(stringArray);
+            Assert.AreEqual(expectedResult, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+
+        }
+    }
+}
